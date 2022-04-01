40d05df7bb3a69b570501332f533a4ef05fb45d4
javad <javad@localhost.localdomain>
2022-04-01 14:02:44 -0400

    added deployment scripts

---
+++ deploy.sh
@@ -0,0 +1,17 @@
+docker build -t 163494315/multi-client:latest -t 163494315/multi-client:$SHA -f ./client/Dockerfile ./client
+docker build -t 163494315/multi-server:latest -t 163494315/multi-server:$SHA -f ./server/Dockerfile ./server
+docker build -t 163494315/multi-worker:latest -t 163494315/multi-worker:$SHA -f ./worker/Dockerfile ./worker
+
+docker push 163494315/multi-client:latest
+docker push 163494315/multi-server:latest
+docker push 163494315/multi-worker:latest
+
+docker push 163494315/multi-client:$SHA
+docker push 163494315/multi-server:$SHA
+docker push 163494315/multi-worker:$SHA
+
+
+kubectl apply -f k8s
+kubectl set image deployments/server-deployment server=163494315/multi-server:$SHA
+kubectl set image deployments/client-deployment server=163494315/multi-client:$SHA
+kubectl set image deployments/worker-deployment server=163494315/multi-worker:$SHA
